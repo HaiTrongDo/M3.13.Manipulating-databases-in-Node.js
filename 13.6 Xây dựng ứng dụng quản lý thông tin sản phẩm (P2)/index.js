e71b6f887f3107756426c4c4001d3ed931a0ef20
HaiTrongDo <dotronghai91@gmail.com>
2022-06-13 22:26:02 +0700

    update

--- 13.6 Xây dựng ứng dụng quản lý thông tin sản phẩm (P2)/index.js
+++ 13.6 Xây dựng ứng dụng quản lý thông tin sản phẩm (P2)/index.js
@@ -27,34 +27,64 @@ connection.connect(err => {
     }
 })
 
-const server = http.createServer((req, res) => {
-    if (req.method === "GET") {
-        fs.readFile("./index.html", "utf-8", (err, data) => {
+function loadWebsite(res) {
+    fs.readFile("./index.html", "utf-8", (err, data) => {
+        let selectData = `SELECT * FROM products;`
+        let html = ''
+        connection.query(selectData, (err, results, fields) => {
+            if (err) {
+                console.log(err.message)
+                return;
+            }
+            results.forEach(element => {
+                html += `<tr>
+                                <td>${element.id}</td>
+                                <td>${element.productName}</td>
+                                <td>${element.productPrice}</td>
+                                </tr>`
+            })
+            data = data.replace('{data}', html)
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data)
             res.end();
         })
-    } else {
-        let inputData = ''
-        req.on('data', chunk => {
-            inputData += chunk
+    })
+}
+
+function insertData(req, res) {
+    let inputData = ''
+    req.on('data', chunk => {
+        inputData += chunk
+    })
+    req.on('end', () => {
+        let newProduct = qs.parse(inputData)
+        console.log(newProduct);
+        let sqlInsert = `INSERT INTO products(productName,productPrice) VALUES ('${newProduct.productName}',${Number(newProduct.productPrice)});`;
+        connection.query(sqlInsert, (err, results, fields) => {
+            if (err) {
+                console.log(err.message)
+            }
+            console.log(`new product ${newProduct.productName} has been added to table Products at $${newProduct.productPrice})`)
+            loadWebsite(res)
         })
-        req.on('end', () => {
-            let newProduct = qs.parse(inputData)
-            console.log(newProduct);
-            let sqlInsert = `INSERT INTO products(productName,productPrice) VALUES ('${newProduct.productName}',${Number(newProduct.productPrice)});`;
-            connection.query(sqlInsert, (err,results, fields) => {
-                if (err) {
-                    console.log(err.message)
-                }
-                console.log(`new product ${newProduct.productName} has been added to table Products at $${newProduct.productPrice})`)
-                res.end("Succeed")
-            })
-        })
-        req.on("error", err => {
-            console.log(err.message)
-        })
+    })
+    req.on("error", err => {
+        console.log(err.message)
+    })
+}
+
+const server = http.createServer((req, res) => {
+    if (req.method === "GET") {
+        loadWebsite(res);
     }
-}).listen(5000, () => {
-    console.log("server is listening on port 5000")
-})
+    else
+        {
+            insertData(req, res);
+        }
+    }
+).
+    listen(5000, () => {
+        console.log("server is listening on port 5000")
+    })
+
+
